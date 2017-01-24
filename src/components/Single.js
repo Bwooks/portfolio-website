fd08662219f282cc59043834db2e0f6e7f812956
Brooks Dulla <brooksdulla@gmail.com>
2017-01-24 17:56:58 -0500

    Working on alternate styling. Added some more of my projects.

--- src/components/Single.js
+++ src/components/Single.js
@@ -7,20 +7,23 @@ export default class Single extends Component{
         const src = require(`file-loader!../../public/assets/${this.props.project}.png`);
         const imgFirst = (
             <div className="single">
-        <img src={src} width="300px" height="300px"/>
+                <div className="single_wrapper">
+                <img src={src} width="300px" height="300px" style={{position:"relative",left:"-50px"}}/>
         <p>But also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
             software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
             </div>
         );
         const imgSecond = (
             <div className="single">
-                <img src={src} width="300px" height="300px"/>
+                <div className="single_wrapper" >
                 <p>But also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
                     software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <img src={src} width="300px" height="300px" style={{position:"relative",right:"-50px"}}/>
+
+                </div>
             </div>
         );
-        return(
-            imgFirst
-        )
+        return(this.props.index % 2 === 0 ? imgFirst : imgSecond);
     }
 }
