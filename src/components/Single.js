f426f9803c6b6a28ecbd856123f9f5e5b3719250
Brooks Dulla <brooksdulla@gmail.com>
2017-01-24 15:46:46 -0500

    Improved mobile responsiveness and working on general styling.

--- src/components/Single.js
+++ src/components/Single.js
@@ -8,8 +8,6 @@ export default class Single extends Component{
         const imgFirst = (
             <div className="single">
         <img src={src} width="300px" height="300px"/>
-        <p>But also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-            software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
         );
         const imgSecond = (
