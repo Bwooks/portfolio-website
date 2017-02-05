12a3b3033e1dceb927984e4ea2dff8e22ab54080
Brooks Dulla <brooksdulla@gmail.com>
2017-02-05 00:32:41 -0500

    Overlay on each project image that gives a brief project summary and links to its github repo and live demo. Mobile responsiveness still needs work.

--- src/components/Single.js
+++ src/components/Single.js
@@ -5,13 +5,20 @@ import React,{Component} from "react";
 export default class Single extends Component{
     render(){
         const src = require(`file-loader!../../public/assets/${this.props.project}.png`);
-        const imgFirst = (
-            <div className="single">
-        <img src={src} width="100%" height="100%"/>
-            </div>
-        );
         return(
-            imgFirst
+            <div className="single">
+                <img className="single_img" src={src}/>
+                <div className="single_overlay">
+                    <div className="overlay_info">
+                        <h2>Title</h2>
+                        <p className="overlay_description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                        <div className="overlay_buttons">
+                            <button className="overlay_demo">Live Demo</button>
+                            <button className="overlay_github">Github</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
