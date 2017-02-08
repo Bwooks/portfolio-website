7fff80b114623704af51d69eb731772023f05ea2
Brooks Dulla <brooksdulla@gmail.com>
2017-02-07 23:45:59 -0500

    Improved mobile responsiveness and more content added.

--- src/components/About.js
+++ src/components/About.js
@@ -8,13 +8,9 @@ const About = (props) =>{
         <a id="about"></a>
             <div className="container">
             <h1>About me</h1>
-            <p className="about_p1">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.
-
+            <p className="about_p1">
             </p>
             <p className="about_p2">
-                Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!
-
-                Download Theme
             </p>
             </div>
         </section>
