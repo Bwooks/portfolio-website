e8d63b6ba530d482bd45d07ad9bb9c469ad2e188
Brooks Dulla <brooksdulla@gmail.com>
2017-03-01 15:15:30 -0500

    Changed format of JSON file, projects section now shows github, codepen or nothing depending on their availability.

--- src/components/Single.js
+++ src/components/Single.js
@@ -5,8 +5,11 @@ import React,{Component} from "react";
 export default class Single extends Component{
     render(){
         const live = this.props.project.live;
-        const github = this.props.project.github;
+        const code = this.props.project.github || this.props.project.codepen;
         const src = require(`file-loader!../../public/assets/${this.props.project.name}.png`);
+            const fullDescription = this.props.project.description.reduce((partA,partB)=>{
+                return partA + "\n" + partB;
+        });
         return(
             <div className="single">
             <a id="projects"></a>
@@ -14,10 +17,13 @@ export default class Single extends Component{
                 <div className={`single_overlay ${this.props.project.name}_overlay`}>
                     <div className="overlay_info">
                         <h2>{this.props.project.name}</h2>
-                        <p className="overlay_description">{this.props.project.description}</p>
+                        <p className="overlay_description">{fullDescription}</p>
                         <div className="overlay_links">
-                            <a className="overlay_demo" href={live} target="_blank">Live Demo</a>
-                            <a className="overlay_github" href={github} target="_blank">Github</a>
+                            <a className="overlay_demo" href={code} target="_blank">Live Demo</a>
+                            <a className={`overlay_code ${typeof code === "undefined" ? "private": "show"}`} href={code} target="_blank">{
+                                /codepen/.test(code) ? "Codepen" : "Github"
+                            }
+                            </a>
                         </div>
                     </div>
                 </div>
